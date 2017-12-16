1c70cd1b21ff4c5699bc88f28e552f7a488466e6
deedkeeper <deedkeeper@gmail.com>
2017-12-16 17:44:40 +0800

    添加项目文件。

---
+++ DataStructVisual_Graph/resource.h
@@ -0,0 +1,38 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 DataStructVisual_Graph.rc 使用
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_DataStructVisuaTYPE         130
+#define ID_GRAPH_OPEN                   32771
+#define ID_GRAPH_SAVE                   32772
+#define ID_32773                        32773
+#define ID_GRAPH_CLEAR                  32774
+#define ID_32775                        32775
+#define ID_32776                        32776
+#define ID_32777                        32777
+#define ID_32778                        32778
+#define ID_32779                        32779
+#define ID_32780                        32780
+#define ID_32781                        32781
+#define ID_GRAPH_VERTEX_ADD             32782
+#define ID_GRAPH_VERTEX_DELETE          32783
+#define ID_GRAPH_VERTEX_INFO            32784
+#define ID_GRAPH_EDGE_ADD               32785
+#define ID_GRAPH_EDGE_DELETE            32786
+#define ID_GRAPH_EDGE_SETWEIGHT         32787
+#define ID_GRAPH_EDGE_INFO              32788
+#define ID_BUTTON32797                  32797
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32799
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
